c5b21378b5989f96f7f93df6f01d156b30920c75
renuc <renuchhimpa42@gmail.com>
2024-07-09 12:42:12 +0530

    earning page design set with adapter

--- app/src/main/java/com/wits/grofast_vendor/Details/EarningPage.java
+++ app/src/main/java/com/wits/grofast_vendor/Details/EarningPage.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.wits.grofast_vendor.Adapter.EarningAdapter;
 import com.wits.grofast_vendor.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EarningPage extends AppCompatActivity {
+    RecyclerView recyclerView;
+    EarningAdapter earningAdapter;
+    LinearLayoutManager linearLayoutManager;
+    List<Map<String, Object>> item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,29 @@ public class EarningPage extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.back);
         setContentView(R.layout.activity_earning_page);
 
+        //Resent Cart Item
+        recyclerView = findViewById(R.id.total_earning_recycleview);
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        item = new ArrayList<>();
+        loadItem();
+        earningAdapter = new EarningAdapter(getApplicationContext(), item);
+        recyclerView.setAdapter(earningAdapter);
+
+
+    }
+
+    private void loadItem() {
+        Map<String, Object> item1 = new HashMap<>();
+        item1.put("Id", "5");
+        item1.put("Amount", "4");
+
+        Map<String, Object> item2 = new HashMap<>();
+        item2.put("Id", "50");
+        item2.put("Amount", "4");
+
+        item.add(item1);
+        item.add(item2);
     }
 
     @Override
